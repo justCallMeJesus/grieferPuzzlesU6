be8eaad7ef717981d5355c5ff8a1845942981a8e
Maciek26 <maciek.kierszka@ict-scouts.ch>
2026-05-02 22:01:20 +0200

    added winning screen logic

--- Assets/_Scripts/GameOverZone.cs
+++ Assets/_Scripts/GameOverZone.cs
@@ -1,12 +1,38 @@
 using UnityEngine;
+using Mirror;
 
-public class GameOverZone : MonoBehaviour
+public class GameOverZone : NetworkBehaviour
 {
+    public GameObject BlueWon;
+    public GameObject GreenWon;
+    public GameObject RedWon;
+    public GameObject YellowWon;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<PlayerManager>() != null)
-        {
-            Debug.Log("Game Over");
-        }
+        if (!isServer) return;
+
+        PlayerManager player = other.GetComponent<PlayerManager>();
+        if (player == null) return;
+
+        int connId = player.connectionToClient != null ? player.connectionToClient.connectionId : -1;
+        Debug.Log($"Game Over triggered by: Connection ID {connId}");
+
+        RpcShowWinScreen(connId);
+    }
+
+    [ClientRpc]
+    private void RpcShowWinScreen(int connId)
+    {
+        if (connId == 0)
+            BlueWon.SetActive(true);
+        else if (connId == 1)
+            GreenWon.SetActive(true);
+        else if (connId == 2)
+            RedWon.SetActive(true);
+        else if (connId == 3)
+            YellowWon.SetActive(true);
+        else
+            Debug.Log($"Unknown player with Connection ID {connId} hit the Game Over zone.");
     }
 }
